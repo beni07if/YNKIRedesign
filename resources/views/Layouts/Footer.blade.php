b39e89cb355bf55f1bb5b5f66445185de30798e0
beni07if <beni07if@gmail.com>
2021-12-08 15:34:01 +0700

    update auth

--- resources/views/Layouts/Footer.blade.php
+++ resources/views/Layouts/Footer.blade.php
@@ -46,7 +46,7 @@
                     <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
                     <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
                     <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
-                    <a href="{{route('staf.index')}}" style="color: black;"></i>Login</a>
+                    <!-- <a href="{{route('staf.index')}}" style="color: black;"></i>Login</a> -->
                 </div>
             </div>
 
