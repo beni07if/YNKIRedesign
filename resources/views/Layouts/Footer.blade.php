b469f6e72cc8c40223ba22684538f15806c37113
beni07if <beni07if@gmail.com>
2022-02-23 11:39:50 +0700

    update footer

--- resources/views/Layouts/Footer.blade.php
+++ resources/views/Layouts/Footer.blade.php
@@ -16,7 +16,7 @@
     <div class="container">
         <div class="row">
 
-            <div class="col-lg-4 col-md-6 footer-contact">
+            <div class="col-lg-3 col-md-6 footer-contact">
                 <!-- <h3>YNKI</h3> -->
                 <img src="assets/img/logo-ynki/logo-ynki-terbaru.png" style="background-color: #5F9EA0; width:200px;" alt="">
                 <p>
@@ -27,16 +27,17 @@
                 </p>
             </div>
 
-            <div class="col-lg-4 col-md-6 footer-links">
+            <div class="col-lg-5 col-md-6 footer-links">
                 <!-- <h4>Layanan Kami</h4>
                 <ul>
                     <li><i class="bx bx-chevron-right"></i> <a href="#">Home</a></li>
                     <li><i class="bx bx-chevron-right"></i> <a href="#">About us</a></li>
                     <li><i class="bx bx-chevron-right"></i> <a href="#">Services</a></li>
                 </ul> -->
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3989.8166845807646!2d109.31833541526026!3d-0.05188253554095343!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e1d5926ff9397b3%3A0x90663a125c803dde!2sYayasan%20Natural%20Kapital!5e0!3m2!1sid!2sid!4v1636959219278!5m2!1sid!2sid" frameborder="0" style="border:0; width: 100%; height: 290px;" allowfullscreen></iframe>
             </div>
 
-            <div class="col-lg-4 col-md-6 footer-links">
+            <div class="col-lg-4 col-md-6 footer-links" style="padding-left: 100px;">
                 <!-- <h4>Our Social Networks</h4> -->
                 <h4>Sosial Media YNKI</h4>
                 <p>Kunjungi sosial media kami..</p>
