99656b665d7ab533e7979b324bfbf0bb8632e272
beni07if <beni07if@gmail.com>
2022-06-15 16:55:11 +0700

    update width maps

--- resources/views/FPLM/Fplm.blade.php
+++ resources/views/FPLM/Fplm.blade.php
@@ -26,7 +26,7 @@
 
         <div class="row">
 
-            <div class="col-lg-5 d-flex flex-column justify-content-center align-items-stretch  order-2 order-lg-1">
+            <div class="col-lg-6 d-flex flex-column justify-content-center align-items-stretch  order-2 order-lg-1">
 
                 <div class="content">
                     <h3>Inisiatif Penerapan Pendekatan Lanskap pada Pembangunan Desa</h3>
@@ -80,7 +80,7 @@
             </div>
 
             <!-- maps service  -->
-            <div class="col-lg-7 align-items-stretch order-1 order-lg-2 img"><iframe width="950" height="600" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" title="Landuse Desa Selat Remis" src="//beni07if.maps.arcgis.com/apps/Embed/index.html?webmap=216fea1dc49e4965833a83c8487f8781&extent=109.0514,-0.4068,109.3049,-0.2648&home=true&zoom=true&previewImage=false&scale=true&search=true&searchextent=true&details=true&legendlayers=true&active_panel=details&basemap_gallery=true&disable_scroll=false&theme=light"></iframe></div>
+            <div class="col-lg-6 align-items-stretch order-1 order-lg-2 img"><iframe width="900" height="600" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" title="Landuse Desa Selat Remis" src="//beni07if.maps.arcgis.com/apps/Embed/index.html?webmap=216fea1dc49e4965833a83c8487f8781&extent=109.0514,-0.4068,109.3049,-0.2648&home=true&zoom=true&previewImage=false&scale=true&search=true&searchextent=true&details=true&legendlayers=true&active_panel=details&basemap_gallery=true&disable_scroll=false&theme=light"></iframe></div>
 
             <!-- <div class="col-lg-6 align-items-stretch order-1 order-lg-2 img" style='background-image: url("assets/img/why-us.png");' data-aos="zoom-in" data-aos-delay="150">&nbsp;</div> -->
             <!-- end maps service  -->
