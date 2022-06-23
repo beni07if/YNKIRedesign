3b117158c463304d8b3146e559d6caca90d70cc0
beni07if <beni07if@gmail.com>
2022-06-23 15:03:06 +0700

    update new dashboard mapsss

--- resources/views/FPLM/fplm04.blade.php
+++ resources/views/FPLM/fplm04.blade.php
@@ -12,72 +12,23 @@
     <div class="container-fluid" data-aos="fade-up">
 
         <div class="row">
-            <div class="content">
-                <h3>Ancaman Perampasan Wilayah Adat dan Deforestasi Masih Nyata - Desa Kinipan</h3>
-                <p>
 
-                    <iframe width="399" height="246" seamless frameborder="0" scrolling="no" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vTBcMeJ-8l5ULAXdaqkpZPjGvhsmkvmM4W8yW28_HZ1P60zFOUBk4T61PjvTaS_wPxkwQhP0UAwdkxf/pubchart?oid=788819742&amp;format=interactive"></iframe>
-                    <iframe width="406" height="251" seamless frameborder="0" scrolling="no" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vTBcMeJ-8l5ULAXdaqkpZPjGvhsmkvmM4W8yW28_HZ1P60zFOUBk4T61PjvTaS_wPxkwQhP0UAwdkxf/pubchart?oid=76667578&amp;format=interactive"></iframe>
-                    <!-- <blockquote class="instagram-media" data-instgrm-permalink="https://www.instagram.com/p/Ce8iggQpFUR/?utm_source=ig_embed&amp;utm_campaign=loading" data-instgrm-version="14" style=" background:#FFF; border:0; border-radius:3px; box-shadow:0 0 1px 0 rgba(0,0,0,0.5),0 1px 10px 0 rgba(0,0,0,0.15); margin: 1px; max-width:540px; min-width:326px; padding:0; width:99.375%; width:-webkit-calc(100% - 2px); width:calc(100% - 2px);">
-                    </blockquote>
-                    <script async src="//www.instagram.com/embed.js"></script> -->
-                </p>
-            </div>
 
-            <!-- <div class="col-lg-4 d-flex flex-column justify-content-flex-start align-items-stretch  order-2 order-lg-1">
+            <div class="col-lg-4 d-flex flex-column justify-content-flex-start align-items-stretch  order-2 order-lg-1">
 
                 <div class="content">
-                    <h3>Inisiatif Penerapan Pendekatan Lanskap pada Pembangunan Desa</h3>
+                    <h3>Ancaman Perampasan Wilayah Adat dan Deforestasi Masih Nyata - Desa Kinipan</h3>
                     <p>
-                        Perencanaan tata guna lahan partisipatif tingkat desa merupakan faktor penting dalam proses pembangunan tingkat desa. Pendekatan ini dapat meningkatkan kapasitas masyarakat dan pemerintah untuk menilai kebutuhan saat ini dan perancangan pembangunan di masa depan. Hal ini terkait dengan definisi desa sebagai kesatuan masyarakat hukum yang memiliki batas wilayah yang berwenang untuk mengatur dan mengurus urusan pemerintahan (UU No. 6 Tahun 2014 Tentang Desa).
 
-                        Tata guna lahan tingkat desa akan terkait dengan lintas level kewenangan dan kebijakan yang harus diakomodasi, seperti adanya batasan pemanfaatan terkait kawasan hutan, kebijakan perlindungan ekosistem gambut dan mangrove maupun batasan pada praktik penggunaan dan pemanfaatannya.
-
-                        Lebih jauh tidak sedikit desa yang berada di sekitar kawasan hutan, lindung dan konservasi seringkali menjadi penghambat dalam pembangunan sosial ekonomi dan infrastruktur. Dalam panduan perencanaan pembangunan desa, perlindungan lingkungan termasuk pelestarian alam dan keanekaragaman hayati termasuk dalam klaster belanja pembangunan desa bidang Pemberdayaan Masyarakat. Kegiatan yang dapat diprogramkan antara lain penghijauan, pengelolaan hutan bakau, perlindungan lahan gambut, perlindungan mata air, pembersihan dan perawatan aliran sungai, perlindungan terumbu karang dan lainnya sesuai dengan kondisi desa.
-
-                        Pemerintah melalui Permendesa PDT dan Transmigrasi nomor 13 tahun 2020, pemerintah mendorong program Pemetaan potensi dan sumber daya lahan pembangunan desa akan berkontribusi pada rancangan adanya peta rancangan tata guna lahan desa dalam rangka menuju perencanaan desa serta pencapaian SDGs desa. Lebih lanjut pemetaan ini menjadi outcome bagi hadirnya intervensi program atau proyek dari otoritas di atasnya (supra desa) antara lain pemerintah pusat, provinsi dan kabupaten.
-
-                        #yayasannaturalkapital
-                        #democracyfornature
-                        #tatagunalahan
-                        <br>
-                        Sumber dan Kontributor (penulis)
+                        <iframe width="399" height="246" seamless frameborder="0" scrolling="no" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vTBcMeJ-8l5ULAXdaqkpZPjGvhsmkvmM4W8yW28_HZ1P60zFOUBk4T61PjvTaS_wPxkwQhP0UAwdkxf/pubchart?oid=788819742&amp;format=interactive"></iframe>
+                        <iframe width="406" height="251" seamless frameborder="0" scrolling="no" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vTBcMeJ-8l5ULAXdaqkpZPjGvhsmkvmM4W8yW28_HZ1P60zFOUBk4T61PjvTaS_wPxkwQhP0UAwdkxf/pubchart?oid=76667578&amp;format=interactive"></iframe>
+                        <!-- <blockquote class="instagram-media" data-instgrm-permalink="https://www.instagram.com/p/Ce8iggQpFUR/?utm_source=ig_embed&amp;utm_campaign=loading" data-instgrm-version="14" style=" background:#FFF; border:0; border-radius:3px; box-shadow:0 0 1px 0 rgba(0,0,0,0.5),0 1px 10px 0 rgba(0,0,0,0.15); margin: 1px; max-width:540px; min-width:326px; padding:0; width:99.375%; width:-webkit-calc(100% - 2px); width:calc(100% - 2px);">
+                    </blockquote>
+                    <script async src="//www.instagram.com/embed.js"></script> -->
                     </p>
                 </div>
 
-                <div class="accordion-list">
-                    <ul>
-                        <li>
-                            <a data-bs-toggle="collapse" class="collapse" data-bs-target="#accordion-list-1"><span>01</span> Non consectetur a erat nam at lectus urna duis? <i class="bx bx-chevron-down icon-show"></i><i class="bx bx-chevron-up icon-close"></i></a>
-                            <div id="accordion-list-1" class="collapse show" data-bs-parent=".accordion-list">
-                                <p>
-                                    Feugiat pretium nibh ipsum consequat. Tempus iaculis urna id volutpat lacus laoreet non curabitur gravida. Venenatis lectus magna fringilla urna porttitor rhoncus dolor purus non.
-                                </p>
-                            </div>
-                        </li>
-
-                        <li>
-                            <a data-bs-toggle="collapse" data-bs-target="#accordion-list-2" class="collapsed"><span>02</span> Feugiat scelerisque varius morbi enim nunc? <i class="bx bx-chevron-down icon-show"></i><i class="bx bx-chevron-up icon-close"></i></a>
-                            <div id="accordion-list-2" class="collapse" data-bs-parent=".accordion-list">
-                                <p>
-                                    Dolor sit amet consectetur adipiscing elit pellentesque habitant morbi. Id interdum velit laoreet id donec ultrices. Fringilla phasellus faucibus scelerisque eleifend donec pretium. Est pellentesque elit ullamcorper dignissim. Mauris ultrices eros in cursus turpis massa tincidunt dui.
-                                </p>
-                            </div>
-                        </li>
-
-                        <li>
-                            <a data-bs-toggle="collapse" data-bs-target="#accordion-list-3" class="collapsed"><span>03</span> Dolor sit amet consectetur adipiscing elit? <i class="bx bx-chevron-down icon-show"></i><i class="bx bx-chevron-up icon-close"></i></a>
-                            <div id="accordion-list-3" class="collapse" data-bs-parent=".accordion-list">
-                                <p>
-                                    Eleifend mi in nulla posuere sollicitudin aliquam ultrices sagittis orci. Faucibus pulvinar elementum integer enim. Sem nulla pharetra diam sit amet nisl suscipit. Rutrum tellus pellentesque eu tincidunt. Lectus urna duis convallis convallis tellus. Urna molestie at elementum eu facilisis sed odio morbi quis
-                                </p>
-                            </div>
-                        </li>
-
-                    </ul>
-                </div>
-
-            </div> -->
+            </div>
             <!-- maps service  -->
             <div class="col-lg-8 align-items-stretch order-1 order-lg-2 img">
                 <!-- map clasic -->
