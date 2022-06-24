bc4f5583db63df6ef84b7985a703f75cbbc0a14c
beni07if <beni07if@gmail.com>
2022-06-24 19:12:28 +0700

    update csS

--- resources/views/Layouts/HeadStyle.blade.php
+++ resources/views/Layouts/HeadStyle.blade.php
@@ -5,8 +5,7 @@
 <meta content="" name="description">
 <meta content="" name="keywords">
 
-<link href="{{ asset('assets/css/style.css') }}" rel="stylesheet">
-<link href="{{ asset('assets/css/aos.css')}}" rel="stylesheet">
+
 <!-- Favicons -->
 
 <link href="{{ asset('assets/img/favicon.png') }}" rel="icon">
@@ -26,11 +25,12 @@
 <link href="{{ asset('assets/vendor/swiper/swiper-bundle.min.css') }}" rel="stylesheet">
 
 <!-- Template Main CSS File -->
+<link href="{{ URL::asset('assets/css/style.css') }}" rel="stylesheet">
+<link href="{{ asset('assets/css/aos.css')}}" rel="stylesheet">
 
 <!-- style utk slider  -->
 <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"> -->
 <!-- end style utk slider  -->
-
 
 <!-- =======================================================
   * Template Name: Arsha - v4.6.0
